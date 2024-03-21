16f11333b143800b1e73d3d217dbdaac67a0bdba
AmJuSi <Amelie.Singler@student.uibk.ac.at>
2024-03-21 17:53:40 +0100

    icon oben bei browserleiste favicon hinzufügen (userbild als vorschauicon)

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([-42.11861111111, 171.32694444444], 13);
+var map = L.map('map').setView([-42.118611111111, 171.32694444444], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([-42.11861111111, 171.32694444444]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+marker.bindPopup("<b>Pancake Rocks</b><br>Here you can find the Pancake Rocks.").openPopup();
