a3864fede78023677652c056a0043451cd1ff455
AmJuSi <Amelie.Singler@student.uibk.ac.at>
2024-03-21 17:45:07 +0100

    kooridnaten für punkt und popup anpassen

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([47.426944444444, 11.4216666666671771], 13);
+var map = L.map('map').setView([-42.11861111111, 171.32694444444], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([47.426944444444, 11.4216666666671771]).addTo(map);
+var marker = L.marker([-42.11861111111, 171.32694444444]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
