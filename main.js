8bac77c1c9382df8e14acda854d3d72d2840f457
AmJuSi <Amelie.Singler@student.uibk.ac.at>
2024-04-11 16:08:45 +0200

    backticks popup

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let lat = -42.118611111111;
 let lng = 171.32694444444;
-let zoom = 19;
+let zoom = 13;
 
-let map = L.map('map').setView([lat, lng], 13);
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: zoom,
+    maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 let marker = L.marker([lat, lng]).addTo(map);
-marker.bindPopup("<b>Pancake Rocks</b><br>Here you can find the Pancake Rocks.").openPopup();
+marker.bindPopup(`<b>Pancake Rocks</b><br>Here you can find the Pancake Rocks.`).openPopup();
