110c2993d444948ab3932ed3db6bf456af18450d
AmJuSi <Amelie.Singler@student.uibk.ac.at>
2024-04-11 16:05:21 +0200

    variable für zoom

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let lat = -42.118611111111;
 let lng = 171.32694444444;
+let zoom = 19;
 
 let map = L.map('map').setView([lat, lng], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: zoom,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 let marker = L.marker([lat, lng]).addTo(map);
